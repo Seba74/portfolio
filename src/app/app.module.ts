10dfa62204a1e9d46a6e8cf9d58eb493557c16e2
Johan Guevara <johanguevara1221@gmail.com>
2023-10-22 16:53:21 -0300

    Fix Animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,11 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { WorksPageComponent } from './pages/works-page/works-page.component';
 import { MobileLayoutComponent } from './layouts/mobile-layout/mobile-layout.component';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, DesktopLayoutComponent, MobileLayoutComponent, HomePageComponent, AboutPageComponent, WorksPageComponent],
-  providers: [provideAnimations()],
+  imports: [BrowserModule, DesktopLayoutComponent, MobileLayoutComponent, HomePageComponent, AboutPageComponent, WorksPageComponent, BrowserAnimationsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
